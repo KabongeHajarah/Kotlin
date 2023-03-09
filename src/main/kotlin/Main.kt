88ae56e3802cf8677a9f8839a268bd3268176ab0
KaboneHajarah <Kabongehajarah@gmail.com>
2023-03-09 08:50:24 +0300

    commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,69 @@
+import kotlin.math.min
+
+fun main() {
+Calculations(arrayOf(10,11,12,13))
+
+
+
+
+
+
+
+
+var calculations= Calculator(12,13,12.0,4)
+   var addition=calculations.addition(10,20)
+println(addition)
+calculations.Subtraction(10,12)
+    calculations.addition(11,12)
+    calculations.Divide(12,20)
+
+}
+fun names(){
+    var fruits=("apple,lemon,berrys,pine")
+    var colors=("black,yellow,red")
+var longer= fruits.get(0)
+
+
+
+}
+
+fun Calculations (age:Array<Int>){
+    val Stats=arrayOf(10,11,12,13)
+    var max1= Stats.max()
+    var min2=Stats.min()
+    var averg=Stats.average()
+
+
+
+}
+
+
+
+
+
+fun new (){
+var greeting= arrayOf("hello","hi",)
+    var people= arrayOf("Bella","Kenny")
+
+}
+class Calculator (var Add:Int,var Subtract:Int, var Divide:Double,var Product:Int){
+    fun addition(x:Int ,y:Int){
+var addition1= x+y
+    println(addition1)
+
+    }
+fun Subtraction(a:Int,b:Int){
+var Subtraction2= a-b
+    println(Subtraction2)
+
+
+}
+fun Divide( a:Int,b:Int){
+    var divide= a/b
+    println(divide)
+}
+fun Multiplicatione( a:Int,b:Int){
+    var multiply= a*b
+    println(multiply)
+}
+}
